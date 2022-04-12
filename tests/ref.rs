061927c656fafe6a2a3c7f820b9cfce8913f86e0
Lucy <lucy@absolucy.moe>
2022-04-11 21:57:26 -0400

    ✅ Add an `emref` test

--- tests/ref.rs
+++ tests/ref.rs
@@ -1,9 +1,17 @@
-use encrypted_ref::eref;
+use encrypted_ref::{emref, eref};
 
 #[test]
 fn encrypted_ref_string() {
 	let string = String::from("hello world!");
 	let string_ref = eref(&string);
 	println!("Normal string ref: {}", &string);
+	println!("Encrypted string ref: {}", &*string_ref);
+}
+
+#[test]
+fn encrypted_mut_ref_string() {
+	let mut string = String::from("hello world!");
+	let mut string_ref = emref(&mut string);
+	string_ref.push_str(" hello from encrypted mut ref!");
 	println!("Encrypted string ref: {}", &*string_ref);
 }
